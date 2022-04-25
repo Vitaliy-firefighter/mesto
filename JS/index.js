12511edaa9a43a4f18636b94c148ce9c0deeffb1
Vitaliy Zakharov <firefighter-zakharov@yandex.ru>
2022-04-25 19:23:02 +0300

    исправлены разметка, доработан JS

--- JS/index.js
+++ JS/index.js
@@ -1,39 +1,35 @@
 const popup = document.querySelector('.popup');
-const popupLink = document.querySelector('.profile__edit');
-const popupClose = document.querySelector('.popup__close');
+const popupLink = document.querySelector('.profile__button-edit');
+const popupClose = document.querySelector('.popup__button-close');
 const buttonSave = document.querySelector('.popup__save');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
+let names = document.querySelector('.profile__names');
+let doer = document.querySelector('.profile__doer');
 
 
 function openedPopup() {
     popup.classList.toggle('popup_opened'); 
+    nameInput.value = names.textContent;
+    jobInput.value = doer.textContent;
 }
 
 
 popupLink.addEventListener('click', openedPopup);
-
 popupClose.addEventListener('click', openedPopup);
+
 
 let formElement = document.querySelector('.popup__form');
 
-let nameInput = document.querySelector('#nameInput');
 
-let jobInput = document.querySelector('#jobInput');
-
-let names = document.querySelector('.profile__names');
-
-let doer = document.querySelector('.profile__doer');
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
-  nameInput.getAttribute('value');
-  jobInput.getAttribute('value');
-
   names.textContent = nameInput.value;
   doer.textContent = jobInput.value;
+
+  openedPopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
-
-buttonSave.addEventListener('click', openedPopup);
-
